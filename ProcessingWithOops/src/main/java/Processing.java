ec8d4f31780c29048b8977329cdc5f3b69b82215
Shalini R <jayanthishalini12@gmail.com>
2024-04-29 16:07:55 +0530

    Refactored Variables

--- ProcessingWithOops/src/main/java/Processing.java
+++ ProcessingWithOops/src/main/java/Processing.java
@@ -5,7 +5,7 @@ public class Processing extends PApplet{
     public static final int WIDTH = 720;
     public static final int HEIGHT = 720;
     public static final int DIAMETER = 10;
-    public static final int POSITION_X = 0;
+    public static final int INITIAL_POSITION_X = 0;
     public static final int NUM_OF_BALLS = 4;
 
     Ball[] balls = new Ball[NUM_OF_BALLS];
@@ -21,7 +21,7 @@ public class Processing extends PApplet{
         for (int ballNum = 0; ballNum < NUM_OF_BALLS; ballNum++) {
             int positionY = (ballNum+1)*HEIGHT/5;
             int speed =ballNum+1;
-            Ball ball = new Ball(POSITION_X,positionY,DIAMETER,speed);
+            Ball ball = new Ball(INITIAL_POSITION_X,positionY,DIAMETER,speed);
             balls[ballNum]=ball;
         }
     }
